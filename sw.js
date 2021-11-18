8ee0bb56c0487f28080ab0a59bbbc8b8dfd84a29
root <root@cvm-3iqxkwe2da225.novalocal>
2021-11-19 00:42:00 +0800

    支持PWA渐进式应用

--- sw.js
+++ sw.js
@@ -1,80 +1,63 @@
-const version = 1;
-const cacheStorageKey = "testCache-" + version;
-
-const cacheList = [
-"./", 
-"./game_list.json",
-"./index.html", 
-"./game.html",
-"./404.html",
-"./manifest.json", 
-"./css/game.css", 
-"./css/index.css",  
-"./icons/72x72.png", 
-"./icons/96x96.png", 
-"./icons/128x128.png", 
-"./icons/144x144.png", 
-"./icons/152x152.png", 
-"./icons/192x192.png", 
-"./icons/256x256.png", 
-"./icons/384x384.png", 
-"./icons/512x512.png", 
-"./js/joystick.js", 
-"./js/jsnes.min.js", 
-"./js/nes_btn.js", 
-"./js/pwa.js",
-"./js/nes-embed.js",
-"./js/nipplejs.min.js",
-"./js/jsnes.min.js.map",
-"./lib/axios.min.js",
-"./lib/vue.min.js",
-"./lib/axios.min.map",
-];
-
-self.addEventListener("install", function(e) {
-//  console.log("Cache event!");
-  e.waitUntil(
-    caches.open(cacheStorageKey).then(function(cache) {
-//      console.log("Adding to Cache:", cacheList);
-      return cache.addAll(cacheList);
-    })
-
+//全部缓存模式
+self.addEventListener('install', function (event) {
+  event.waitUntil(
+    caches.open('cache-v1').then(function (cache) {
+      cache
+        .addAll
+      // levels 11–20
+      ([
+        './',
+      ]);
+      return cache
+        .addAll
+      // core assets and levels 1–10
+      ([
+        './',
+      ]);
+    }),
   );
 });
 
-self.addEventListener("activate", event => {
-//  console.log("Activate event");
-  event.waitUntil(
-    caches
-      .keys()
-      .then(cacheNames => {
-        return cacheNames.filter(cacheName => cacheStorageKey !== cacheName);
-      })
-      .then(cachesToDelete => {
-        return Promise.all(
-          cachesToDelete.map(cacheToDelete => {
-            return caches.delete(cacheToDelete);
+//磁盘缓存
+self.addEventListener('fetch', function (event) {
+  event.respondWith(
+    caches.open('cache-sd').then(function (cache) {
+      return cache.match(event.request).then(function (response) {
+        return (
+          response ||
+          fetch(event.request).then(function (response) {
+            cache.put(event.request, response.clone());
+            return response;
           })
         );
-      })
-      .then(() => {
-//        console.log("Clients claims.");
-        self.clients.claim();
-      })
+      });
+    }),
   );
 });
 
-self.addEventListener("fetch", function(e) {
-  e.respondWith(
-    caches.match(e.request).then(function(response) {
-      return response || fetch(e.request);
-    })
+//刷新磁盘缓存
+self.addEventListener('fetch', function (event) {
+  event.respondWith(
+    caches.open('cache-sd').then(function (cache) {
+      return fetch(event.request).then(function (response) {
+        cache.put(event.request, response.clone());
+        return response;
+      });
+    }),
   );
 });
 
-self.addEventListener("message", event => {
-  if (event.data === "skipWaiting") {
-//    console.log("Skip waiting!");
-    self.skipWaiting();
-  }
+//缓存刷新后同步数据
+self.addEventListener('fetch', function (event) {
+  event.respondWith(
+    caches.open('cache-v2').then(function (cache) {
+      return cache.match(event.request).then(function (response) {
+        var fetchPromise = fetch(event.request).then(function (networkResponse) {
+          cache.put(event.request, networkResponse.clone());
+          return networkResponse;
+        });
+        return response || fetchPromise;
+      });
+    }),
+  );
 });
