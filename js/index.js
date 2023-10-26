3a9e0438df75b01641bf0f03fd1523aed48a3e72
root <root@cvm-3iqxkwe2da225.novalocal>
2023-10-27 06:51:34 +0800

    update

--- js/index.js
+++ js/index.js
@@ -1,33 +1,43 @@
-// 页面载入设置
+// 严格模式
+"use strict"
+// 页面载入
 document.onreadystatechange = function() {
 	if (document.readyState === "interactive") {
 		document.body.classList.remove("is-loading");
 	}
 }
+// 数据容器
+let data;
+// 取得资源
+const search = document.getElementById('search');
+const cdtopbt = document.getElementById("cd-top");
+const title = document.title;
 // 数据加载
 function intdata() {
 	fetch('./list.json', {
 			methods: 'GET',
-			cache: 'no-cache'
+			cache: 'force-cache',
+			mode: 'same-origin',
+			integrity: 'sha256-61KQ7jxXtD+GotQw8ZcJDG4UP67whfRq8ThEsowFu18='
 		})
 		.then(response => {
 			return response.json();
 		})
 		.then(datas => {
 			let item = "";
-			let app = document.getElementById('app');
-			let search = document.getElementById('search').value.replace(/(^\s*)|(\s*$)/g, '');
-			if (search === '') {
+			const app = document.getElementById('app');
+			const searchV = search.value.replace(/(^\s*)|(\s*$)/g, '');
+			if (searchV === '') {
 				data = datas;
 			} else {
-				data = datas.filter(array => array.n.match(search) || array.v.match(search) || array.c.match(
-					search));
+				data = datas.filter(array => array.n.match(searchV) || array.v.match(searchV) || array.c.match(
+					searchV));
 				if (data.length === 0) {
 					app.classList.add('sapp');
 					app.innerHTML = '<h1>什么东东都没有，换个词试试丫！</h1>';
 					return;
 				}
-			}
+			};
 			for (let j = 0; j < data.length; j++) {
 				let span1 = data[j].v != '' ? '<span class="p2">' + data[j].v + '</span>' : '';
 				let span2 = data[j].c != '' ? '<span class="p3">' + data[j].c + '</span>' : '';
@@ -45,22 +55,40 @@ function intdata() {
 		})
 }
 intdata();
-//打开游戏页面
+//打开游戏
 function opgame(i) {
 	window.open('./play.html?=' + i, '_self');
 }
-//网站标题自动判断
-let title = document.title;
-
-function istitle() {
-	let isHidden = document.hidden;
-	if (isHidden) {
-		//当窗口不可见
+//标题判断
+document.addEventListener('visibilitychange', function() {
+	if (document.hidden) {
+		//窗口不可见
 		document.title = '(つ ェ ⊂)我藏好了哦~';
 	} else {
-		//当窗口可见
+		//窗口可见
 		document.title = '(*゜ロ゜)ノ被发现了~';
-		setTimeout("document.title=title", 3000);
+		setTimeout(() => {
+			document.title = title;
+		}, 3000);
 	}
-};
-document.addEventListener('visibilitychange', istitle);
+})
+//返回顶部
+window.onscroll = function() {
+	if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+		cdtopbt.className = "cdtopVis";
+	} else {
+		cdtopbt.className = "cdtopHid";
+	}
+}
+
+function cdTop() {
+	document.body.scrollTop = 0;
+	document.documentElement.scrollTop = 0;
+}
+
+// 回车搜索
+window.onkeydown = function() {
+	if (window.event && window.event.keyCode == 13 && search == document.activeElement) {
+		intdata();
+	}
+}
