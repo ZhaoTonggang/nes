41690c9aaeded582ba3cd98b885d0060f3f96f64
root <root@cvm-3iqxkwe2da225.novalocal>
2023-11-30 04:06:15 +0800

    add old

---
+++ old/js/index.js
@@ -0,0 +1,117 @@
+// 严格模式
+"use strict";
+// 页面载入
+if (window.top != window) {
+	alert('当您看到这条提示意味着：您所访问的网站正在恶意调用本站资源，本站对偷盗资源的行为0容忍，点击确认跳转正版体验。');
+	window.open('https://nes.heheda.top', '_self');
+} else if (window.location.href.indexOf('index') > -1) {
+	window.open('./', '_self');
+};
+// 数据容器
+let data;
+let timeout = null;
+// 取得资源
+const search = document.getElementById('search');
+const cdtopbt = document.getElementById("cd-top");
+const title = document.title;
+// 数据加载
+const intdata = () => {
+	fetch('./list.json', {
+			methods: 'GET',
+			cache: 'force-cache',
+			mode: 'same-origin'
+		})
+		.then(response => {
+			return response.json();
+		})
+		.then(datas => {
+			let item = "";
+			const app = document.getElementById('app');
+			const searchV = search.value.replace(/(^\s*)|(\s*$)/g, '');
+			if (searchV === '') {
+				data = datas;
+			} else {
+				data = datas.filter(array => array.n.match(searchV) || array.v.match(searchV) || array.c.match(
+					searchV));
+				if (data.length === 0) {
+					app.classList.add('sapp');
+					app.innerHTML = '<h1>什么东东都没有，换个词试试丫！</h1>';
+					return;
+				};
+			};
+			for (let j = 0; j < data.length; j++) {
+				let span1 = data[j].v != '' ? '<span class="p2">' + data[j].v + '</span>' : '';
+				let span2 = data[j].c != '' ? '<span class="p3">' + data[j].c + '</span>' : '';
+				let opgamev = data[j].v != '' ? "'" + data[j].v + "'" : false;
+				item +=
+					'<div class="item" onclick="opgame(' + opgamev + ',\'' + data[j].n + '\',\'' + data[j].i +
+					'\')">' +
+					'<div class="img_box"><img src="../imgs/' + data[j].i + '.png" title="' + data[j].n +
+					'" alt="' +
+					data[j].n + '">' + span1 + span2 + '</div><p class="p1">' + data[j].n + '</p></div>';
+			};
+			app.classList.remove('sapp');
+			app.innerHTML = item;
+		})
+		.catch(err => {
+			document.getElementById('apph').innerHTML = '获取数据失败<br />请尝试“异常修复”<br />[错误代码:404]';
+			console.error('[404]错误日志：', err);
+		})
+};
+intdata();
+//打开游戏
+const opgame = (v, n, i) => {
+	window.open(encodeURI('./play/?v=' + v + '&n=' + n + '&i=' + i), '_self');
+};
+//标题判断
+window.addEventListener('visibilitychange', () => {
+	if (document.hidden) {
+		//窗口不可见
+		document.title = '(つ ェ ⊂)我藏好了哦~';
+	} else {
+		//窗口可见
+		document.title = '(*゜ロ゜)ノ被发现了~';
+		setTimeout(() => {
+			document.title = title;
+		}, 3000);
+	};
+});
+//返回顶部
+const cdTop = () => {
+	document.documentElement.scrollTop = 0;
+	document.body.scrollTop = 0;
+};
+// 监听屏幕滚动
+window.addEventListener('scroll', () => {
+	if (timeout !== null) {
+		clearTimeout(timeout);
+	}
+	timeout = setTimeout(() => {
+		let scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+		// 返回顶部
+		if (scrollTop > 100) {
+			cdtopbt.className = "cdtopVis";
+		} else {
+			cdtopbt.className = "cdtopHid";
+		};
+	}, 500);
+});
+// 回车搜索
+window.onkeydown = () => {
+	if (window.event && window.event.keyCode == 13 && search == document.activeElement) {
+		intdata();
+	};
+};
+// 程序修复
+const fixsys = () => {
+	const yesfix = confirm('确认运行修复程序吗？系统将向云端请求最新的数据包，这将有效应对绝大多数异常情况。');
+	if (yesfix == true) {
+		window.location.replace(window.location.href);
+	}
+}
+// 移除遮罩
+document.onreadystatechange = () => {
+	if (document.readyState === "interactive") {
+		document.body.classList.remove("is-loading");
+	};
+};
---
+++ old/js/indexeddb.js
@@ -0,0 +1,276 @@
+// 格式化时间
+Date.prototype.Format = function(fmt) {
+	var o = {
+		"M+": this.getMonth() + 1,
+		"d+": this.getDate(),
+		"h+": this.getHours(),
+		"m+": this.getMinutes(),
+		"s+": this.getSeconds(),
+		"q+": Math.floor((this.getMonth() + 3) / 3),
+		"S": this.getMilliseconds()
+	};
+	if (/(y+)/.test(fmt)) fmt = fmt.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
+	for (var k in o)
+		if (new RegExp("(" + k + ")").test(fmt)) fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : ((
+			"00" + o[k]).substr(("" + o[k]).length)));
+	return fmt;
+};
+
+// 打开并创建数据库
+function openDB(dbName, version = 1) {
+	return new Promise((resolve, reject) => {
+		let indexedDB = window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB || window.msIndexedDB;
+		let db;
+		// 打开并创建数据库
+		let request = indexedDB.open(dbName, version);
+		// 成功回调
+		request.onsuccess = function(event) {
+			// 数据库对象
+			db = event.target.result;
+			console.log("打开成功");
+			resolve(db);
+		};
+		// 失败回调
+		request.onerror = function(event) {
+			console.log("打开失败");
+		};
+		// 更新数据
+		request.onupgradeneeded = function(event) {
+			console.log("onupgradeneeded");
+			// 数据库对象
+			db = event.target.result;
+			let objectStore;
+			// 创建数据库
+			objectStore = db.createObjectStore("users", {
+				// 设置主键
+				keyPath: "uuid"
+				// 实现自增
+				// autoIncrement:true
+			});
+			// 创建索引
+			objectStore.createIndex("uuid", "uuid", {
+				unique: true
+			});
+			objectStore.createIndex("name", "name", {
+				unique: false
+			});
+			objectStore.createIndex("age", "age", {
+				unique: false
+			});
+		};
+	});
+}
+
+// 插入数据
+function addDate(db, storeName, data) {
+	let request = db
+		// 指定操作模式
+		.transaction([storeName], "readwrite")
+		// 创建数据库
+		.objectStore(storeName)
+		.add(data);
+	request.onsuccess = function(event) {
+		console.log("数据写入成功");
+	}
+	request.onerror = function(event) {
+		console.log("数据写入失败");
+	}
+}
+
+// 通过主键查询数据
+function getDataByKey(db, storeName, key) {
+	return new Promise((resolve, reject) => {
+		// 事务
+		let transaction = db.transaction([storeName]);
+		// 创建对象
+		let objectStore = transaction.objectStore(storeName);
+		// 通过主键获取数据
+		let request = objectStore.get(key);
+		request.onerror = function(event) {
+			console.log("事务失败");
+		};
+		request.onsuccess = function(event) {
+			console.log("主键查询结果：", request.result);
+		};
+	});
+}
+
+// 通过游标查询数据
+function cursorGetBata(db, storeName) {
+	let list = [];
+	let store = db
+		// 事务
+		.transaction(storeName, "readwrite")
+		// 仓库对象
+		.objectStore(storeName);
+	// 指针对象
+	let request = store.openCursor();
+	// 游标开启成功,逐行读数据
+	request.onsuccess = function(e) {
+		let cursor = e.target.result;
+		if (cursor) {
+			// 必须要检查
+			list.push(cursor.value);
+			// 遍历了存储对象中的所有内容
+			cursor.continue();
+		} else {
+			console.log("游标读取的数据：", list);
+		}
+	}
+}
+
+// 通过索引查询数据
+function getDataByIndex(db, storeName, indexName, indexValue) {
+	let store = db
+		.transaction(storeName, "readwrite")
+		.objectStore(storeName);
+	let request = store.index(indexName).get(indexValue);
+	request.onerror = function() {
+		console.log("事务失败");
+	};
+	request.onsuccess = function(e) {
+		let result = e.target.result;
+		console.log("索引查询结果：", result);
+	};
+}
+
+// 通过索引和游标查询数据
+function cursorGetDataByIndex(db, storeName, indexName, indexValue) {
+	let list = [];
+	let store = db
+		// 仓库对象
+		.transaction(storeName, "readwrite")
+		.objectStore(storeName);
+	let request = store
+		// 索引对象
+		.index(indexName)
+		// 指针对象
+		.openCursor(IDBKeyRange.only(indexValue));
+	request.onsuccess = function(e) {
+		let cursor = e.target.result;
+		if (cursor) {
+			// 必须要检查
+			list.push(cursor.value);
+			// 遍历储存对象中的所有内容
+			cursor.continue();
+		} else {
+			console.log("游标查询结果：", list);
+		}
+	};
+	request.onerror = function(e) {};
+}
+
+// 通过索引和游标分页查询
+function cursorGetDataByIndexAndPage(db, storeName, indexName, indexValue, page, pageSize) {
+	let list = [];
+	// 计数器
+	let counter = 0;
+	// 是否跳过多少条查询
+	let advanced = true;
+	// 仓库对象
+	let store = db.transaction(storeName, "readwrite")
+		.objectStore(storeName);
+	let request = store
+		// 索引对象
+		.index(indexName)
+		// 指针对象
+		.openCursor(IDBKeyRange.only(indexValue));
+	request.onsuccess = function(e) {
+		let cursor = e.target.result;
+		if (page > 1 && advanced) {
+			advanced = false;
+			// 跳过多少条
+			cursor.advance((page - 1) * pageSize);
+			return;
+		}
+		if (cursor) {
+			// 必须要检查
+			list.push(cursor.value);
+			counter++;
+			if (counter < pageSize) {
+				// 遍历储存对象中的所有内容
+				cursor.continue();
+			} else {
+				cursor = null;
+				console.log("分页查询结果：", list);
+			}
+		} else {
+			console.log("分页查询结果：", list);
+		}
+	};
+	request.onerror = function(e) {}
+}
+
+// 更新数据
+function updateDB(db, storeName, data) {
+	let request = db
+		// 事务对象
+		.transaction([storeName], "readwrite")
+		// 仓库对象
+		.objectStore(storeName)
+		.put(data);
+	request.onsuccess = function() {
+		console.log("数据更新成功");
+	};
+	request.onerror = function() {
+		console.log("数据更新失败");
+	};
+}
+
+// 通过主键删除数据
+function deleteDB(db, storeName, id) {
+	let request = db
+		.transaction([storeName], "readwrite")
+		.objectStore(storeName)
+		.delete(id);
+	request.onsuccess = function() {
+		console.log("数据删除成功");
+	};
+	request.onerror = function() {
+		console.log("数据删除失败");
+	}
+}
+
+// 通过索引和游标删除数据
+function cursorDelete(db, storeName, indexName, indexValue) {
+	let store = db.transaction(storeName, "readwrite").objectStore(storeName);
+	let request = store
+		// 索引对象
+		.index(indexName)
+		// 指针对象
+		.openCursor(IDBKeyRange.only(indexValue));
+	request.onsuccess = function(e) {
+		let cursor = e.target.result;
+		let deleteRequest;
+		if (cursor) {
+			// 请求删除当前项
+			deleteRequest = cursor.delete();
+			deleteRequest.onerror = function() {
+				console.log("游标删除失败");
+			};
+			deleteRequest.onsuccess = function() {
+				console.log("游标删除成功");
+			};
+			cursor.continue();
+		}
+	};
+}
+
+
+// 关闭数据库
+function closeDB(db) {
+	db.close();
+	console.log("数据库已关闭");
+}
+
+// 删除数据库
+function deleteDBAll(dbName) {
+	console.log(dbName);
+	let deleteRequest = window.indexedDB.deleteDatabase(dbName);
+	deleteRequest.onerror = function(event) {
+		console.log("删除失败");
+	};
+	deleteRequest.onsuccess = function(event) {
+		console.log("删除成功");
+	}
+}
